95897defe5425db050a20b0f6d73556e41f09dd8
carlos <carlos.shilei@gmail.com>
2018-08-27 10:26:41 +0800

    安装leanCloud storage以及封装

---
+++ src/leanCloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'Q9akOzPhf8koNx08FcRDyiDj-gzGzoHsz';
+var APP_KEY = 'e3hOm0ea8fkJYAP9K5anDzyI';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
